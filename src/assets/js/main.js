bd32bf5b1d03d824dfd4a347be738317e5fcf274
Diogo Silva Ferreira <diogosilvaferreira@gmail.com>
2016-11-19 19:09:02 -0200

    add two sports

---
+++ src/assets/js/main.js
@@ -0,0 +1,25 @@
+;(function() {
+  document.getElementById("subscribe-footer").addEventListener("click", function() {
+    fbq('track','CompleteRegistration');
+  });
+
+  var formHeader = document.querySelector('.subscribe-form-footer');
+  formHeader.onsubmit = valid;
+
+  function valid() {
+    var isValid = !0,
+        inputEmail = document.querySelector('.subscribe-email');
+    
+    isValid = checkEmail(inputEmail.value);
+    
+    if (!isValid) {
+      inputEmail.classList.add('form-erro');
+    }
+    return isValid
+  }
+
+  function checkEmail(email) {
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email)
+  }
+})();
